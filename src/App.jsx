42c8c0b37f619851d454a2509f90763ad3020570
Chandan <dbgchandan441@gmail.com>
2024-10-27 21:48:03 +0530

    separting user and admin data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useContext, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
+import { AuthContext } from "./context/AuthProvider";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext)
+  console.log(authData);
+  
 
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage()
-  },)
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      setUser('admin');
+    } else if (email == "user@me.com" && password == "123") {
+      setUser('user');
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
   
   return (
     <>
-      <Login />
-      {/* <EmployeeDashboard /> */}
-      {/* <AdminDashboard /> */}
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   );
 };
